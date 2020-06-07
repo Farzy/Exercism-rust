eaced42b9bc4a1c369370874d936214312eb0d0b
Farzad FARID <farzad.farid@kapten.com>
2020-06-07 21:16:51 +0200

    proverb: Improve following other contributions

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut proverb = String::new();
-    if !list.is_empty() {
-        for i in 0..(list.len() - 1) {
-            proverb.push_str(&format!(
-                "For want of a {} the {} was lost.\n",
-                list[i],
-                list[i + 1]
-            ));
-        }
-        proverb.push_str(&format!("And all for the want of a {}.", list[0]));
+    if list.is_empty() {
+        return String::new();
     }
-    proverb
+
+    list.windows(2)
+        .map(|items| format!("For want of a {} the {} was lost.", items[0], items[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
