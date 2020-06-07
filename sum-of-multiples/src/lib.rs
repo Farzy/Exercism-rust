a049bcc3ac9af33fa7ca1a447fee1ae85628d8bc
Farzad FARID <farzy@farzy.org>
2020-06-08 00:04:59 +0200

    sum-of-multiples: Fix for factor=0

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -3,7 +3,8 @@ use std::collections::HashSet;
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples: HashSet<u32> = HashSet::new();
 
-    for factor in factors {
+    // Remove factor==0 otherwise our infinite iterator never ends
+    for factor in factors.iter().filter(|x| **x != 0) {
         let numbers = (1..)
             .map(|x| *factor * x)
             .take_while(|x| *x < limit)
