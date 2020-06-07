740c31f8fbb708ae7007a7e66622d29fc51d7f0d
Farzad FARID <farzy@farzy.org>
2020-06-07 19:07:36 +0200

    proverb: Improve emptiness test

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn build_proverb(list: &[&str]) -> String {
     let mut proverb = String::new();
-    if list.len() != 0 {
+    if !list.is_empty() {
         for i in 0..(list.len() - 1) {
             proverb.push_str(&format!(
                 "For want of a {} the {} was lost.\n",
