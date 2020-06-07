ecc996d500c5a50cc7c7f834a88df8f74b370a53
Farzad FARID <farzy@farzy.org>
2020-06-07 18:46:33 +0200

    beer-song: Implement

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    if n != 0 {
+        let plural = match n {
+            1 => (String::from(""), String::from("it"), String::from("no more bottles")),
+            2 => (String::from("s"), String::from("one"), String::from("1 bottle")),
+            _ => (String::from("s"), String::from("one"), format!("{} bottles", n-1)),
+        };
+        return format!("{} bottle{} of beer on the wall, {} bottle{} of beer.\nTake {} down and pass it around, {} of beer on the wall.\n", n, plural.0, n, plural.0, plural.1, plural.2)
+    } else {
+        return String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..start+1)
+        .rev()
+        .map(|x| { verse(x) })
+        .collect::<Vec<String>>()
+        .join("\n")
 }
