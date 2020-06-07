07f779201654b26c48688817b6372649065e1fbe
Farzad FARID <farzad.farid@kapten.com>
2020-06-07 22:42:24 +0200

    sum-of-multiples: WIP. Must delete duplicates.

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut sum: u32 = 0;
+    let mut multiples: Vec<u32> = Vec::new();
+
+    for factor in factors {
+        let mut numbers = (1..)
+            .map(|x| *factor * x)
+            .take_while(|x| *x < limit)
+            .collect::<Vec<u32>>();
+        multiples.append(&mut numbers);
+    }
+    println!("For factor {:?} we found numbers {:?}", factors, multiples);
+    sum
 }
