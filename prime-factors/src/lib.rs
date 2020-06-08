a6f9ca1655c22eb3a53a5a39f2ae47de77ed30c4
Farzad FARID <farzad.farid@kapten.com>
2020-06-08 18:03:12 +0200

    prime-factors: Double speed by not dividing by even numbers except 2

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -2,7 +2,7 @@
 
 extern crate test;
 
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors_full_iter(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
     let mut number = n;
 
@@ -19,10 +19,38 @@ pub fn factors(n: u64) -> Vec<u64> {
     factors
 }
 
+// This version is twice as fast as factors_full_iter
+pub fn factors(n: u64) -> Vec<u64> {
+    let mut factors: Vec<u64> = Vec::new();
+    let mut number = n;
+
+    let mut factor: u64 = 2;
+    loop {
+        while number % factor == 0 {
+            factors.push(factor);
+            number /= factor;
+        }
+        if number == 1 || factor > number {
+            break;
+        }
+        match factor {
+            2 => factor = 3,
+            _ => factor += 2,
+        }
+    }
+
+    factors
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
+
+    #[bench]
+    fn bench_factors_full_iter(b: &mut Bencher) {
+        b.iter(|| factors_full_iter(93_819_012_551));
+    }
 
     #[bench]
     fn bench_factors(b: &mut Bencher) {
