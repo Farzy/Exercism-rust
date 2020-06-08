0e79192de65833bc09cd984ddf6b51e408672bc7
Farzad FARID <farzad.farid@kapten.com>
2020-06-08 17:54:15 +0200

    prime-factors: Fix bench name

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
     let mut number = n;
@@ -13,4 +17,15 @@ pub fn factors(n: u64) -> Vec<u64> {
     }
 
     factors
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_factors(b: &mut Bencher) {
+        b.iter(|| factors(93_819_012_551));
+    }
 }
