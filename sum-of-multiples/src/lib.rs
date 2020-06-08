468d9b63fc199c96b4664fdf80c23dc8e15148ff
Farzad FARID <farzad.farid@kapten.com>
2020-06-08 11:51:26 +0200

    sum-of-multiples: Optimize by not using HashSet but only iterators

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+pub fn sum_of_multiples2(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples: HashSet<u32> = HashSet::new();
 
     // Remove factor==0 otherwise our infinite iterator never ends
@@ -12,4 +12,10 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
         multiples.extend(numbers);
     }
     multiples.iter().sum::<u32>()
+}
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1..limit)
+        .filter(|x| factors.iter().any(|f| *f != 0 && *x % *f == 0))
+        .sum::<u32>()
 }
