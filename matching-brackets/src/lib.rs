72e320d5ab43e152a9f6147041ae73538db8c6dd
Farzad FARID <farzad.farid@kapten.com>
2020-06-18 21:25:07 +0200

    matching-brackets: Partial solution

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,24 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut paren_cnt = 0;
+    let mut curly_cnt = 0;
+    let mut bracket_cnt = 0;
+
+    for c in string.chars() {
+        match c {
+            '(' => paren_cnt += 1,
+            ')' => paren_cnt -= 1,
+            '{' => curly_cnt += 1,
+            '}' => curly_cnt -= 1,
+            '[' => bracket_cnt += 1,
+            ']' => bracket_cnt -= 1,
+            _ => (),
+        }
+        if paren_cnt < 0 || curly_cnt < 0 || bracket_cnt < 0 {
+            return false;
+        }
+    }
+    if paren_cnt != 0 || curly_cnt != 0 || bracket_cnt != 0 {
+        return false;
+    }
+    true
 }
