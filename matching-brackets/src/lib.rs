e66fbef6f7785d0d356f049a04c9cf71c3048651
Farzad FARID <farzad.farid@kapten.com>
2020-06-18 22:15:58 +0200

    matching-brackets: Working solution

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,24 +1,31 @@
-pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut paren_cnt = 0;
-    let mut curly_cnt = 0;
-    let mut bracket_cnt = 0;
+use std::collections::HashMap;
 
-    for c in string.chars() {
-        match c {
-            '(' => paren_cnt += 1,
-            ')' => paren_cnt -= 1,
-            '{' => curly_cnt += 1,
-            '}' => curly_cnt -= 1,
-            '[' => bracket_cnt += 1,
-            ']' => bracket_cnt -= 1,
+pub fn brackets_are_balanced(string: &str) -> bool {
+    // Keep track of open brackets in order
+    let mut open_brackets: Vec<char> = vec![];
+    // Helper for matching closing brackets to the opening one
+    let mut opposite_brackets: HashMap<char, char> = HashMap::new();
+    opposite_brackets.insert(')', '(');
+    opposite_brackets.insert('}', '{');
+    opposite_brackets.insert(']', '[');
+
+    for current_char in string.chars() {
+        match current_char {
+            '(' | '{' | '[' => open_brackets.push(current_char),
+            ')' | '}' | ']' => {
+                if let Some(last_open_bracket) = open_brackets.pop() {
+                    if opposite_brackets.get(&current_char).unwrap() != &last_open_bracket {
+                        // Current closing bracket does not match last opened bracket kind
+                        return false;
+                    }
+                } else {
+                    // There was no open bracket of any kind
+                    return false;
+                }
+            }
             _ => (),
         }
-        if paren_cnt < 0 || curly_cnt < 0 || bracket_cnt < 0 {
-            return false;
-        }
     }
-    if paren_cnt != 0 || curly_cnt != 0 || bracket_cnt != 0 {
-        return false;
-    }
-    true
+    // Check for unbalanced brackets at end of string
+    open_brackets.len() == 0
 }
