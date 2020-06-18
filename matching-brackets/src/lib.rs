5aa1fade7f037152633a83447b430fbfb545ef18
Farzad FARID <farzad.farid@kapten.com>
2020-06-18 22:25:38 +0200

    matching-brackets: More elegant and concise solution

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -5,21 +5,17 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut open_brackets: Vec<char> = vec![];
     // Helper for matching closing brackets to the opening one
     let mut opposite_brackets: HashMap<char, char> = HashMap::new();
-    opposite_brackets.insert(')', '(');
-    opposite_brackets.insert('}', '{');
-    opposite_brackets.insert(']', '[');
+    opposite_brackets.insert('(', ')');
+    opposite_brackets.insert('{', '}');
+    opposite_brackets.insert('[', ']');
 
     for current_char in string.chars() {
         match current_char {
-            '(' | '{' | '[' => open_brackets.push(current_char),
+            '(' | '{' | '[' => open_brackets.push(*opposite_brackets.get(&current_char).unwrap()),
             ')' | '}' | ']' => {
-                if let Some(last_open_bracket) = open_brackets.pop() {
-                    if opposite_brackets.get(&current_char).unwrap() != &last_open_bracket {
-                        // Current closing bracket does not match last opened bracket kind
-                        return false;
-                    }
-                } else {
-                    // There was no open bracket of any kind
+                if open_brackets.pop() != Some(current_char) {
+                    // Current closing bracket does not match last opened bracket kind
+                    // or there is no open bracked
                     return false;
                 }
             }
@@ -27,5 +23,5 @@ pub fn brackets_are_balanced(string: &str) -> bool {
         }
     }
     // Check for unbalanced brackets at end of string
-    open_brackets.len() == 0
+    open_brackets.is_empty()
 }
