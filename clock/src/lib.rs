d51d56e3217d9ba79fc3fccfd81b0933b10b60cc
Farzad FARID <farzad.farid@kapten.com>
2020-06-19 11:07:31 +0200

    clock: Implement

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,22 @@
-pub struct Clock;
+use std::fmt;
 
+// Store time as minutes, implement minimum necessary derives
+#[derive(PartialEq, Debug)]
+pub struct Clock(i32);
+
+// Warning: Do not use '%' as it keep the sign of the dividend
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock((hours * 60 + minutes).rem_euclid(1440))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock((self.0 + minutes).rem_euclid(1440))
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.0 / 60, self.0 % 60)
     }
 }
