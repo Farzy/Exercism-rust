7205117da347fab0371300af7fd2e676a799c1bc
Farzad FARID <farzy@farzy.org>
2020-07-14 23:15:23 +0200

    saddle-points: Refactor
    * Rename variable
    * Fix typos, improve comments

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -7,21 +7,23 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     }
     let mut min_values_by_col: Vec<u64> = vec![];
 
-    // Find largest elements in each row
-    for (r, v) in input.iter().enumerate() {
+    // Find largest elements in each row and smallest value per column
+    // at the same time.
+    for (row_num, row) in input.iter().enumerate() {
         // One time initialisation of min_values_by_col
         if min_values_by_col.is_empty() {
-            min_values_by_col.resize(v.len(), u64::MAX);
+            min_values_by_col.resize(row.len(), u64::MAX);
         }
 
-        if let Some(max_in_row) = v.iter().max() {
-            for (c, point) in v.iter().enumerate() {
-                // Find small value in each column
-                min_values_by_col[c] = min(min_values_by_col[c], *point);
+        if let Some(max_in_row) = row.iter().max() {
+            for (col_num, point) in row.iter().enumerate() {
+                // Find smallest value in each column
+                min_values_by_col[col_num] = min(min_values_by_col[col_num],
+                                                 *point);
 
                 // Collect largest elements in each row
                 if *point == *max_in_row {
-                    max_points_by_row.push((r, c));
+                    max_points_by_row.push((row_num, col_num));
                 }
             }
         } else { // Empty line
@@ -29,11 +31,10 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
         }
     }
 
-    // Now cleanup the vector max_points_by_row to keep only those with
-    // minimal value per col
-    max_points_by_row
-        .retain(|&point|
-            input[point.0][point.1] == min_values_by_col[point.1]);
+    // Now cleanup the vector max_points_by_row to keep only those points with
+    // minimal value per col.
+    max_points_by_row.retain(|&point|
+        input[point.0][point.1] == min_values_by_col[point.1]);
 
     max_points_by_row
 }
