00521f0f08849494695409f32528f397baed9298
Farzad FARID <farzy@farzy.org>
2020-07-18 21:25:53 +0200

    dot-dsl: Implement minimum structs to passe one test

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,9 +1,41 @@
 pub mod graph {
-    pub struct Graph;
+    use std::collections::HashMap;
+
+    pub struct Graph {
+        pub nodes : Vec<graph_items::node::Node>,
+        pub edges : Vec<graph_items::edge::Edge>,
+        pub attrs : HashMap<String, String>,
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: vec![],
+                edges: vec![],
+                attrs: Default::default(),
+            }
+        }
+    }
+
+    pub mod graph_items {
+        pub mod edge {
+            pub struct Edge;
+
+            impl Edge {
+                pub fn new() -> Self {
+                    unimplemented!("Construct a new Edge struct.");
+                }
+            }
+        }
+
+        pub mod node {
+            pub struct Node;
+
+            impl Node {
+                pub fn new() -> Self {
+                    unimplemented!("Construct a new Node struct.");
+                }
+            }
         }
     }
 }
