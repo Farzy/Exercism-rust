152417f7919aaf454e29fc1ec522c9dfcf72060b
Farzad FARID <farzy@farzy.org>
2020-09-05 23:43:36 +0200

    Start implementing say

---
+++ say/src/lib.rs
@@ -0,0 +1,64 @@
+
+pub fn encode(n: u64) -> String {
+    if n == 0 {
+        "zero".into()
+    } else if n < 10 {
+        unit(n).into()
+    } else if n < 20 {
+        ten_something(n).into()
+    } else if n < 100 {
+        if n % 10 == 0 {
+            dozen(n / 10).into()
+        } else {
+            format!("{}-{}", dozen(n / 10), unit(n % 10))
+        }
+    } else
+    {
+        unimplemented!("Say {} in English.", n);
+    }
+}
+
+fn unit(n: u64) -> &'static str {
+    match n {
+        0 => "",
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        5 => "five",
+        6 => "six",
+        7 => "seven",
+        8 => "eight",
+        9 => "nine",
+        _ => panic!("Units greater than 9"),
+    }
+}
+
+fn ten_something(n: u64) -> &'static str {
+    match n {
+        1 => "eleven",
+        2 => "twelve",
+        3 => "thirteen",
+        4 => "fourteen",
+        5 => "fifteen",
+        6 => "sixteen",
+        7 => "seventeen",
+        8 => "eighteen",
+        9 => "nineteen",
+        _ => panic!(format!("Invalid number: {}", n)),
+    }
+}
+
+fn dozen(n: u64) -> &'static str {
+    match n {
+        2 => "twenty",
+        3 => "thirty",
+        4 => "fourty",
+        5 => "fifty",
+        6 => "sixty",
+        7 => "seventy",
+        8 => "eighty",
+        9 => "ninety",
+        _ => panic!(format!("Invalid dozen: {}", n)),
+    }
+}
