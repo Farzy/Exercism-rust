9579a574949a0619d9ab657aec71ebf51c6a344e
Farzad FARID <farzad.farid@kapten.com>
2020-09-11 11:23:47 +0200

    Implement rle encoding

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,5 +1,39 @@
+fn find_repetition(source: &str) -> usize {
+    assert_ne!(source.len(), 0);
+    let mut source_iter = source.chars();
+    let c = source_iter.next().unwrap();
+    let mut count = 1;
+    for next_c in source_iter {
+        if next_c == c {
+            count += 1;
+        } else {
+            break;
+        }
+    }
+    count
+}
+
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut source_iter = source.chars().peekable();
+    let mut encoded = String::new();
+    loop {
+        match source_iter.next() {
+            None => break,
+            Some(current_char) => {
+                let mut count = 1;
+                while source_iter.peek() == Some(&current_char) {
+                    count += 1;
+                    source_iter.next();
+                }
+                if count == 1 {
+                    encoded.push(current_char);
+                } else {
+                    encoded.push_str(&format!("{}{}", count, current_char));
+                }
+            }
+        }
+    }
+    encoded
 }
 
 pub fn decode(source: &str) -> String {
