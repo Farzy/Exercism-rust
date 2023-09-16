02b4f49b7b7f068cff09079d55c2c98a9a275afa
Farzad FARID <farzy@farzy.org>
2023-09-16 12:05:11 +0200

    Resubmit gigasecond with new crate

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+const GIGASECONDS: i64 = 1_000_000_000;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    start + Duration::new(GIGASECONDS, 0)
 }
