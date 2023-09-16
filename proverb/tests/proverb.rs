31d4dae9b5f8ed2f5a58bd23bd974d4c0879942f
Farzad FARID <farzy@farzy.org>
2023-09-16 10:58:05 +0200

    Update all Rust exercises

--- proverb/tests/proverb.rs
+++ proverb/tests/proverb.rs
@@ -3,7 +3,7 @@ use proverb::build_proverb;
 #[test]
 fn test_two_pieces() {
     let input = vec!["nail", "shoe"];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "And all for the want of a nail.",
     ]
@@ -13,9 +13,10 @@ fn test_two_pieces() {
 
 // Notice the change in the last line at three pieces.
 #[test]
+#[ignore]
 fn test_three_pieces() {
     let input = vec!["nail", "shoe", "horse"];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "For want of a shoe the horse was lost.",
         "And all for the want of a nail.",
@@ -25,6 +26,7 @@ fn test_three_pieces() {
 }
 
 #[test]
+#[ignore]
 fn test_one_piece() {
     let input = vec!["nail"];
     let expected = String::from("And all for the want of a nail.");
@@ -32,6 +34,7 @@ fn test_one_piece() {
 }
 
 #[test]
+#[ignore]
 fn test_zero_pieces() {
     let input: Vec<&str> = vec![];
     let expected = String::new();
@@ -39,11 +42,12 @@ fn test_zero_pieces() {
 }
 
 #[test]
+#[ignore]
 fn test_full() {
     let input = vec![
         "nail", "shoe", "horse", "rider", "message", "battle", "kingdom",
     ];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "For want of a shoe the horse was lost.",
         "For want of a horse the rider was lost.",
@@ -57,9 +61,10 @@ fn test_full() {
 }
 
 #[test]
+#[ignore]
 fn test_three_pieces_modernized() {
     let input = vec!["pin", "gun", "soldier", "battle"];
-    let expected = vec![
+    let expected = [
         "For want of a pin the gun was lost.",
         "For want of a gun the soldier was lost.",
         "For want of a soldier the battle was lost.",
