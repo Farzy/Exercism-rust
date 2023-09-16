8046c8bf496d36a5b3b64c87dcba896d4a8b2fb6
Farzad FARID <farzy@farzy.org>
2023-09-16 11:20:13 +0200

    Update all Rust exercises

--- run-length-encoding/tests/run-length-encoding.rs
+++ run-length-encoding/tests/run-length-encoding.rs
@@ -3,22 +3,25 @@ use run_length_encoding as rle;
 // encoding tests
 
 #[test]
-fn test_encode_empty_string() {
+fn encode_empty_string() {
     assert_eq!("", rle::encode(""));
 }
 
 #[test]
-fn test_encode_single_characters() {
+#[ignore]
+fn encode_single_characters() {
     assert_eq!("XYZ", rle::encode("XYZ"));
 }
 
 #[test]
-fn test_encode_string_with_no_single_characters() {
+#[ignore]
+fn encode_string_with_no_single_characters() {
     assert_eq!("2A3B4C", rle::encode("AABBBCCCC"));
 }
 
 #[test]
-fn test_encode_single_characters_mixed_with_repeated_characters() {
+#[ignore]
+fn encode_single_characters_mixed_with_repeated_characters() {
     assert_eq!(
         "12WB12W3B24WB",
         rle::encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
@@ -26,34 +29,40 @@ fn test_encode_single_characters_mixed_with_repeated_characters() {
 }
 
 #[test]
-fn test_encode_multiple_whitespace_mixed_in_string() {
+#[ignore]
+fn encode_multiple_whitespace_mixed_in_string() {
     assert_eq!("2 hs2q q2w2 ", rle::encode("  hsqq qww  "));
 }
 
 #[test]
-fn test_encode_lowercase_characters() {
+#[ignore]
+fn encode_lowercase_characters() {
     assert_eq!("2a3b4c", rle::encode("aabbbcccc"));
 }
 
 // decoding tests
 
 #[test]
-fn test_decode_empty_string() {
+#[ignore]
+fn decode_empty_string() {
     assert_eq!("", rle::decode(""));
 }
 
 #[test]
-fn test_decode_single_characters_only() {
+#[ignore]
+fn decode_single_characters_only() {
     assert_eq!("XYZ", rle::decode("XYZ"));
 }
 
 #[test]
-fn test_decode_string_with_no_single_characters() {
+#[ignore]
+fn decode_string_with_no_single_characters() {
     assert_eq!("AABBBCCCC", rle::decode("2A3B4C"));
 }
 
 #[test]
-fn test_decode_single_characters_with_repeated_characters() {
+#[ignore]
+fn decode_single_characters_with_repeated_characters() {
     assert_eq!(
         "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB",
         rle::decode("12WB12W3B24WB")
@@ -61,19 +70,22 @@ fn test_decode_single_characters_with_repeated_characters() {
 }
 
 #[test]
-fn test_decode_multiple_whitespace_mixed_in_string() {
+#[ignore]
+fn decode_multiple_whitespace_mixed_in_string() {
     assert_eq!("  hsqq qww  ", rle::decode("2 hs2q q2w2 "));
 }
 
 #[test]
-fn test_decode_lower_case_string() {
+#[ignore]
+fn decode_lower_case_string() {
     assert_eq!("aabbbcccc", rle::decode("2a3b4c"));
 }
 
 // consistency test
 
 #[test]
-fn test_consistency() {
+#[ignore]
+fn consistency() {
     assert_eq!(
         "zzz ZZ  zZ",
         rle::decode(rle::encode("zzz ZZ  zZ").as_str())
